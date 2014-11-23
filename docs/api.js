c6b7378955be03bb2a1d942e2fd467f379fbb3c2
Sam Wheeler <Sam@Sams-MacBook-Pro.local>
2014-11-23 16:48:08 +0000

    Repository initial build

---
+++ docs/api.js
@@ -0,0 +1,34 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "pk",
+        "pk.accordian",
+        "pk.carousel",
+        "pk.chart",
+        "pk.checkbox",
+        "pk.color",
+        "pk.colorpicker",
+        "pk.datepicker",
+        "pk.drag",
+        "pk.modal",
+        "pk.notify",
+        "pk.rating",
+        "pk.scroll",
+        "pk.select",
+        "pk.slider",
+        "pk.svg",
+        "pk.switch",
+        "pk.toggle",
+        "pk.tooltip"
+    ],
+    "modules": [
+        "pocketknife"
+    ],
+    "allModules": [
+        {
+            "displayName": "pocketknife",
+            "name": "pocketknife"
+        }
+    ]
+} };
+});
