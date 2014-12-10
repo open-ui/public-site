1dd887fea8140500e2939ebbaf9649981cd3958c
sw4 <sw4@users.noreply.github.com>
2014-12-10 10:52:04 +0000

    Update public-site.js

--- src/js/public-site.js
+++ src/js/public-site.js
@@ -8,61 +8,79 @@ window.onscroll=function(e){
 		pagenav.setAttribute("class",css.replace("navbar-inverse","").trim());
 	}
 };
-/*
+
 function chartData(){
 	return [{
         block: 'fingers',
         category: "Invoices",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 0
     }, {
         block: 'fingers',
         category: "Petrol",
-        'This Month': oui.getRand(0, 9),
-        'Last Month': oui.getRand(1, 8)
+        'This Month': $ui.getRand(0, 9),
+        'Last Month': $ui.getRand(1, 8)
     }, {
         block: 'toes',
         category: "Invoices",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 9
     }, {
         block: 'toes',
         category: "Petrol",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 0
     }, {
         block: 'thumbs',
         category: "Invoices",
-        'This Month': oui.getRand(0, 9),
-        'Last Month': oui.getRand(1, 8)
+        'This Month': $ui.getRand(0, 9),
+        'Last Month': $ui.getRand(1, 8)
     }, {
         block: 'thumbs',
         category: "Petrol",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 9
     }, {
         block: 'fingers',
         category: "Accommodation",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 9
     }, {
         block: 'toes',
         category: "Accommodation",
-        'This Month': oui.getRand(0, 9),
-        'Last Month': oui.getRand(1, 8)
+        'This Month': $ui.getRand(0, 9),
+        'Last Month': $ui.getRand(1, 8)
     }, {
         block: 'thumbs',
         category: "Accommodation",
-        'This Month': oui.getRand(0, 9),
+        'This Month': $ui.getRand(0, 9),
         'Last Month': 0
     }];
 }
-oui.carousel({
+
+$ui.chart({
+	element:document.getElementById('uiLineChart'),
+    type: 'line',
+    data: chartData(),
+    colors: {
+        'Petrol': '#F20081',
+        'Invoices': '#EBBC00',
+        'Accommodation': '#00DECF'
+    },
+    legend: true,
+    axis: {
+        x: 'Last Month',
+        y: 'This Month'
+    },
+    series: 'category'
+});
+/*
+$ui.carousel({
 	element:document.getElementById('carousel'),
 	delay:5000
 });
 
-oui.chart({
+$ui.chart({
 	element:document.getElementById('lineChart'),
     type: 'line',
     data: chartData(),
@@ -78,7 +96,7 @@ oui.chart({
     },
     series: 'category'
 });
-oui.chart({
+$ui.chart({
 	element:document.getElementById('scatterChart'),
     type: 'scatter',
     data: chartData(),
@@ -94,7 +112,7 @@ oui.chart({
     },
     series: 'category'
 });
-oui.chart({
+$ui.chart({
 	element:document.getElementById('areaChart'),
     type: 'area',
     data: chartData(),
@@ -110,7 +128,7 @@ oui.chart({
     },
     series: 'category'
 });
-oui.chart({
+$ui.chart({
 	element:document.getElementById('bubbleChart'),
     type: 'scatter',
     data: chartData(),
@@ -127,7 +145,7 @@ oui.chart({
     },
     series: 'category'
 });
-oui.chart({
+$ui.chart({
 	element:document.getElementById('barChart'),
     type: 'bar',
     data: chartData(),
@@ -143,7 +161,7 @@ oui.chart({
     },
     series: 'category'
 });
-oui.chart({
+$ui.chart({
 	element:document.getElementById('columnChart'),
     type: 'column',
     data: chartData(),
